furiosa-ai/furiosa-server-validation-tool

chore: rename container directory to furiosa-rngd-validator
The container's working directory and ENV variable still referenced
the old repository name. Update both:

- `ENV VALIDATION_DIR` is renamed to `ENV VALIDATOR_DIR`, matching
  `entrypoint.sh`'s already-renamed export.
- The default path becomes `$HOME/furiosa-rngd-validator`.
- The `ENTRYPOINT` JSON form points at the new path.
- `ENV OUTPUT_DIR=$VALIDATION_DIR/outputs` follows the same rename
  to `$VALIDATOR_DIR/outputs`.
- `Makefile` and `README.md` bind-mount targets update from
  `/root/furiosa-server-validation-tool/outputs` to
  `/root/furiosa-rngd-validator/outputs`.

dockerfmt --check passes after the change.

Makefile
@@ -21,7 +21,7 @@ run:
 	docker run --rm -it --privileged \
 	    -v /sys/kernel/debug:/sys/kernel/debug \
 	    -v /lib/modules:/lib/modules:ro \
-	    -v $(PWD)/outputs:/root/furiosa-server-validation-tool/outputs \
+	    -v $(PWD)/outputs:/root/furiosa-rngd-validator/outputs \
 	    -e HF_TOKEN=$(HF_TOKEN) \
 	    -e RUN_TESTS=$(RUN_TESTS) \
 	    $(IMAGE)

furiosa-ai/furiosa-rngd-validator

Makefile
@@ -27,7 +27,7 @@ run:
 	docker run --rm -it --privileged \
 	    -v /sys/kernel/debug:/sys/kernel/debug \
 	    -v /lib/modules:/lib/modules:ro \
-	    -v $(CURDIR)/outputs:/root/furiosa-server-validation-tool/outputs \
+	    -v $(CURDIR)/outputs:/root/furiosa-rngd-validator/outputs \
 	    -e HF_TOKEN \
 	    -e RUN_TESTS=$(RUN_TESTS) \
 	    $(IMAGE)